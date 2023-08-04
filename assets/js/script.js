7b5831490e06db12bb2dbe1738b68755b6ca43bb
Sean Finley <finleysean9@gmail.com>
2023-08-03 21:00:26 -0400

    Ceated starter html css and java, and started making timer function

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+var timerEl = document.querySelector(".timer");
+var getScoresEl = document.getElementById("get-scores");
+
+
+
+function setTimer() {
+    var seconds = 75;
+    var timerInterval = setInterval(function() {
+        if (seconds > 1) {
+            timerEl.textContent = "Time Remaining: " + seconds + " seconds";
+            seconds--;
+        } else if (seconds == 1) {
+            timerEl.textContent = "Time Remaining: " + seconds + " second";
+            seconds--;
+        } else {
+            timerEl.textContent = "Out of Time"
+            clearInterval(timerInterval);
+        }
+
+    }, 1000);
+}
+
+//setTimer();
