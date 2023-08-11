b2a29984289184ff8558684c4cdb496086b44c8f
Sean Finley <finleysean9@gmail.com>
2023-08-10 22:25:02 -0400

    created questions and start of quiz function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 var timerEl = document.querySelector(".timer");
 var getScoresEl = document.getElementById("get-scores");
 var startButton = document.querySelector(".start-button");
-
+var startText = document.getElementById("Start-text")
 
 function startQuiz(){
     setTimer();
-
+    document.getElementById("Start-text").remove()
+    document.querySelector(".start-button").remove()
 }
 
 
@@ -29,6 +30,38 @@ function setTimer() {
     }, 1000);
 }
 
+const Questions = [{
+    q: "What are stings contained within?",
+    a: [{ text: "Parentheses", isCorrect: false },
+    { text: "$", isCorrect: false },
+    { text: "?", isCorrect: false },
+    { text: "Quotation Marks", isCorrect: true }
+    ]
+ 
+},
+{
+    q: "When you create a function, for it to work you must _____.",
+    a: [{ text: "Call it", isCorrect: true},
+    { text: "label it", isCorrect: false },
+    { text: "Delete it", isCorrect: false },
+    { text: "Pull it", isCorrect: false }
+    ]
+ 
+},
+{
+    q: "Arrays contain ______.",
+    a: [{ text: "Numbers", isCorrect: false },
+    { text: "Letters", isCorrect: false },
+    { text: "Symbols", isCorrect: false },
+    { text: "All of the Above", isCorrect: true }
+    ]
+ 
+}
+]
 
+let score = 0
+let thisQuestion = 0
 
+function showQuestions () {
 
+}
