a3b9935dbc5eb635cd3e40184931b131fe9ae330
Sean Finley <finleysean9@gmail.com>
2023-08-11 20:23:17 -0400

    created the function to show questions and added a submit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,18 @@
 var timerEl = document.querySelector(".timer");
 var getScoresEl = document.getElementById("get-scores");
 var startButton = document.querySelector(".start-button");
-var startText = document.getElementById("Start-text")
+var startText = document.getElementById("Start-text");
+var submitButton = document.getElementById("submit-button");
+
+var btn = document.createElement("button");
+btn.textContent = "Submit";
 
 function startQuiz(){
     setTimer();
     document.getElementById("Start-text").remove()
     document.querySelector(".start-button").remove()
+    showQuestions();
+    document.getElementById("submit-button").appendChild(btn);
 }
 
 
@@ -32,7 +38,7 @@ function setTimer() {
 
 const Questions = [{
     q: "What are stings contained within?",
-    a: [{ text: "Parentheses", isCorrect: false },
+    answers: [{ text: "Parentheses", isCorrect: false },
     { text: "$", isCorrect: false },
     { text: "?", isCorrect: false },
     { text: "Quotation Marks", isCorrect: true }
@@ -41,7 +47,7 @@ const Questions = [{
 },
 {
     q: "When you create a function, for it to work you must _____.",
-    a: [{ text: "Call it", isCorrect: true},
+    answers: [{ text: "Call it", isCorrect: true},
     { text: "label it", isCorrect: false },
     { text: "Delete it", isCorrect: false },
     { text: "Pull it", isCorrect: false }
@@ -50,7 +56,7 @@ const Questions = [{
 },
 {
     q: "Arrays contain ______.",
-    a: [{ text: "Numbers", isCorrect: false },
+    answers: [{ text: "Numbers", isCorrect: false },
     { text: "Letters", isCorrect: false },
     { text: "Symbols", isCorrect: false },
     { text: "All of the Above", isCorrect: true }
@@ -63,5 +69,26 @@ let score = 0
 let thisQuestion = 0
 
 function showQuestions () {
+    const question = document.querySelector(".question");
+    const options = document.querySelector(".options");
 
+    question.textContent = Questions[thisQuestion].q;
+    options.innerHTML = ""
+
+    for (let i = 0; i < Questions[thisQuestion].answers.length; i++) {
+        const choicesdiv = document.createElement("div");
+        const choice = document.createElement("input");
+        const choiceLabel = document.createElement("label");
+
+        choice.type="radio";
+        choice.name="answer";
+        choice.value=i;
+
+        choiceLabel.textContent = Questions[thisQuestion].answers[i].text;
+
+        choicesdiv.appendChild(choice);
+        choicesdiv.appendChild(choiceLabel);
+        options.appendChild(choicesdiv);
+    }
 }
+
